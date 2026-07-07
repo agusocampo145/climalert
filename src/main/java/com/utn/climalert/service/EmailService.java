36f28aab49f5b3cb7ebdeaed8242a102e3c00899
Agus Ocampo <agusocampo145@hotmail.com>
2026-07-06 23:31:05 -0300

    Valores pedidos en application.properties + habilitacion de @Async para el envio del email

--- src/main/java/com/utn/climalert/service/EmailService.java
+++ src/main/java/com/utn/climalert/service/EmailService.java
@@ -6,11 +6,13 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 @Slf4j
+@Async("mailExecutor")
 public class EmailService {
 
     private final JavaMailSender mailSender;
